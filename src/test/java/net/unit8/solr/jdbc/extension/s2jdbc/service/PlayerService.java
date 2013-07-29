cf16c92857e7c17244848732a8fec46f26e327fe
kawasima <kawasima1016@gmail.com>
2013-07-29 18:48:02 +0900

    fixes #2

--- src/test/java/net/unit8/solr/jdbc/extension/s2jdbc/service/PlayerService.java
+++ src/test/java/net/unit8/solr/jdbc/extension/s2jdbc/service/PlayerService.java
@@ -2,9 +2,11 @@ package net.unit8.solr.jdbc.extension.s2jdbc.service;
 
 import java.util.List;
 
+import org.seasar.extension.jdbc.name.PropertyName;
 import org.seasar.extension.jdbc.service.S2AbstractService;
 import org.seasar.framework.beans.util.BeanMap;
 import org.seasar.framework.util.tiger.CollectionsUtil;
+import static org.seasar.extension.jdbc.operation.Operations.*;
 
 import net.unit8.solr.jdbc.extension.s2jdbc.entity.Player;
 
@@ -13,14 +15,20 @@ public class PlayerService extends S2AbstractService<Player> {
 
 	public Player find(int id) {
 		BeanMap conditions = new BeanMap();
-		conditions.put("playerId", 1);
+		conditions.put("playerId", id);
 		List<Player> playerList = findByCondition(conditions);
 		if(playerList.size() > 0) {
 			return playerList.get(0);
 		}
 		return null;
 	}
-	
+
+    @Override
+    public List<Player> findAll() {
+        return select()
+                .orderBy(asc(new PropertyName<Long>("playerId"))).getResultList();
+    }
+
 	public List<Player> findBySql() {
 		BeanMap conditions = new BeanMap();
 		List<String> positionList = CollectionsUtil.newArrayList();
